9dfb64c360be8324016a98434515d66724394f19
eliakim pires <eliakimribeiro10@hotmail.com>
2022-10-14 14:51:18 -0300

    123

--- App.js
+++ App.js
@@ -3,7 +3,6 @@ import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import Routes from './src/routes';
 
-
 export default function App() {
   return (
     <NavigationContainer>
